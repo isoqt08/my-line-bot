b51dbfe37724eb0761e4b963a257fb7b75bcea1a
isoqt08 <132338166+isoqt08@users.noreply.github.com>
2023-05-04 16:21:48 +0800

    add gpt

--- api/index.py
+++ api/index.py
@@ -15,6 +15,19 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('0iMXINLZV6RkNb+fgUbYeZsvM3ZYkXUPTTvVl6X610KeMN2SYLionwEmFkmEyV3pYKBjT4QPv6Ci6yX52t17bE9Ja1R62l1Vqn2CwvNxSjxHagi8TSOql/lwc/p+XNWFhTe9nNCurDAH06Lu0370TAdB04t89/1O/w1cDnyilFU=')
 webhook_handler = WebhookHandler('8b312c172bf6e9791e7c54abe218362e')
 
+# 設定 OpenAI API 密鑰
+# openai.api_key = os.environ["OPENAI_API_KEY"]
+openai.api_key = "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"
+
+# 輸入文本
+input_text = "今天天氣很好，請用中文回答。請做一首跟天氣有關的詩"
+
+# 設定 GPT-3.5 模型的檢索引擎
+model_engine = "text-davinci-003"
+
+# 設定生成的文本長度
+output_length = 300
+
 @app.route("/")
 def home():
     return "LINE BOT API Server is running."
@@ -40,10 +53,20 @@ def callback():
 
 @webhook_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+
+    # 使用 GPT-3.5 模型生成文本
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=event.message.text,
+        max_tokens=output_length,
+    )
+
+    # 取得生成的文本
+    output_text = response.choices[0].text.strip()
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
-
+        TextSendMessage(text=output_text))
 
 if __name__ == "__main__":
     app.run()
